f854e81574005b0bdf1f57ae4c648a2f8af59589
Mariano Guezuraga <mguezuraga@gmail.com>
2022-01-07 23:10:57 +0100

    New features:
    - Adding support for fetching multiple facts
    - Adding a CLI option for it

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,85 @@
 use serde::Deserialize;
+use structopt::StructOpt;
 
-#[derive(Deserialize)]
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(short, long, help = "Display multiple cat facts")]
+    multiple: bool,
+}
+
+#[derive(Deserialize, Debug)]
 struct CatFact {
     fact: String,
 }
 
-fn main() -> Result<(), ureq::Error> {
+#[derive(Deserialize, Debug)]
+struct CatFacts {
+    data: Vec<CatFact>,
+}
 
-    let url = "https://catfact.ninja/fact";
-    
-    match ureq::get(&url).call() {
+#[allow(dead_code)]
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+#[allow(dead_code)]
+fn json_from_string() -> String {
+    r#"
+    {
+        "current_page": 1,
+        "data": [
+          {
+            "fact": "A",
+            "length": 1
+          },
+          {
+            "fact": "AB",
+            "length": 2
+          }
+        ]
+    }
+    "#.to_string()
+}
+
+fn fetch_random_facts() -> CatFacts {
+
+    let url = "https://catfact.ninja/facts";
+    match ureq::get(url).call() {
         Ok(response) => {
-            let randomfact: CatFact = response.into_json()?;
-            println!("{}",randomfact.fact);
+            response.into_json().unwrap()
         },
         Err(_) => {
             println!("Something went wrong while fetching facts");
             std::process::exit(1);
         }
     }
-    Ok(())
+}
+
+fn fetch_one_random_fact() -> CatFact {
+    let url = "https://catfact.ninja/fact";
+    match ureq::get(url).call() {
+        Ok(response) => {
+            response.into_json().unwrap()
+        },
+        Err(_) => {
+            println!("Something went wrong while fetching facts");
+            std::process::exit(1);
+        }
+    }
+
+}
+
+fn main()  {
+    let args = Cli::from_args();
+
+    if args.multiple {
+        let cat_facts: CatFacts = fetch_random_facts();
+        for cat_fact in &cat_facts.data {
+            println!("* {}\n", cat_fact.fact);
+        }
+    } else {
+        let cat_fact: CatFact = fetch_one_random_fact();
+        println!("{:#?}", cat_fact.fact);
+    }
+
 }
